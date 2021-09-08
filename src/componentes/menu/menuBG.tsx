cb6d52a104804ef376f464dfded376b1972b80e0
Leonel Velez <leonel-14-velez@hotmail.com>
2021-09-08 14:46:17 -0500

    agregando componente de menu

---
+++ src/componentes/menu/menuBG.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './menuBG.css';
+import { bubble as Menu } from 'react-burger-menu'
+
+
+
+export default class MenuBG extends React.Component<any, any>
+{
+ 
+ constructor(props:any)
+ {
+    super(props);
+ }
+ 
+  render()
+  {
+    return (<>        
+        <div className="container-menu">
+          <Menu pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" } >
+              <div id="items-menu" style={{display:"flex", flexDirection:"column"}}>
+                    {
+                        ["Item uno ","Item numero dos","iterm numero 3"].map((recorre, id)=>{
+                            return ( <p> {recorre} </p>);
+                        })
+                    }
+                </div>         
+          </Menu>
+        </div>
+               
+    </>)
+  }
+}
