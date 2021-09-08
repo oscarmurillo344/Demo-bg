90e9bce9f758716659e8c74ecceb3ea063923bed
Leonel Velez <leonel-14-velez@hotmail.com>
2021-09-08 15:34:15 -0500

    agregando nav bar con el logo del banco guaqaquil

--- src/componentes/menuBG/menuBG.tsx
+++ src/componentes/menuBG/menuBG.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './menuBG.css';
 import { bubble as Menu } from 'react-burger-menu'
-
+import imagen from './../../logo.png'
 
 
 export default class MenuBG extends React.Component<any, any>
@@ -14,7 +14,10 @@ export default class MenuBG extends React.Component<any, any>
  
   render()
   {
-    return (<>        
+    return (<>
+        <nav className="nav-bar" >
+          <img src= {imagen} width="200px" className="logo" ></img>
+        </nav>        
         <div className="container-menu">
           <Menu pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" } >
               <div id="items-menu" style={{display:"flex", flexDirection:"column"}}>
