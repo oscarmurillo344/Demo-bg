df23c36f8d319239a9e9bed7c0c6b12370db0090
Leonel Velez <leonel-14-velez@hotmail.com>
2021-09-10 01:07:32 -0500

    agregando detalle del gridview

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,13 @@ import MenuBG from './componentes/menuBG/menuBG';
 import data from './data.json'
 
 function App() {
-  const columns: GridColDef[] = [
-    {field: 'id', headerName: 'Id', width: 160},
-    {field: 'nombre', headerName: 'Nombre', width: 170},      
-    {field: 'apellido', headerName: 'Apellido', width: 170},
-    {field: 'ci', headerName: 'Ci', width: 170},
-    {field: 'fehaNacimiento', headerName: 'Fecha Nacimiento', width: 170},
-    {field: 'correo', headerName: 'Correo', width: 170},
-    {field: 'fecha', headerName: 'Fecha', width: 170, type:"date" }
+  const columns: any[] = [
+    {dataIndex: 'id', key:'id', title: 'Id', width: 160},
+    {dataIndex: 'nombre', key:'nombre', title: 'Nombre', width: 170},      
+    {dataIndex: 'apellido', key:'apellido', title: 'Apellido', width: 170},
+    {dataIndex: 'ci', key:'ci',  title: 'Ci', width: 170},
+    {dataIndex: 'fehaNacimiento', key:'fehaNacimiento' , title: 'Fecha Nacimiento', width: 170},
+    {dataIndex: 'correo', key:"correo",  title: 'Correo', width: 170},
   ];
 
 
@@ -21,20 +20,20 @@ function App() {
   for(let recorre = 0; recorre<300000; recorre++)
   {
     rows.push({
-      id: recorre+1,
+      key: recorre+1,
+      id:recorre+1,
       nombre: "Nathalie Andrea",
       apellido: "Bohorquez Velez",
       ci: "0932312113",
       fehaNacimiento : "1998/05/03",
       correo: "leonel-14-velez@hotmail.com",
-      fecha: new Date()
 })
   }
   return (
       <> 
         <MenuBG>
           <div className="flex" style={{justifyContent:"center"}}  >
-            <GridViewBG height={525} columns={columns} rows={rows} pagesize={8}  width={1200} 
+            <GridViewBG height={325} columns={columns} rows={rows} pagesize={10}  width={1200} 
               
             />
           </div>
--- src/componentes/gridViewBG/gridViewBG.tsx
+++ src/componentes/gridViewBG/gridViewBG.tsx
@@ -1,5 +1,8 @@
 import React, { CSSProperties, useState } from 'react';
 import { DataGrid, GridColDef, GridCsvExportApi, GridExportCsvOptions, GridFilterItem, GridFilterModel, GridLinkOperator, GridToolbar, GridToolbarContainer, GridToolbarExport, GridToolbarFilterButton } from '@material-ui/data-grid';
+import 'antd/dist/antd.css';
+import { Badge, Dropdown, Space, Table } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
 
 interface RimGridViewProps{
     width:number;
@@ -12,34 +15,46 @@ interface RimGridViewProps{
 
 
 const GridViewBG = (props:RimGridViewProps)=>{
-    const [filtro, setFiltro] = useState(null)
-    let referencia = React.useRef<HTMLDivElement>()
-    const borrarFiltro = (e:any)=>{
-        
-        setFiltro(e)
 
-        
-    }
-    const CustomToolbar = ()=> {
-        return (
-        
-          <GridToolbar></GridToolbar>
-        );
-      }
-      
-      
-    return  (
-        <> 
-            <div style={{width:props.width, height:props.height}} >
-                <DataGrid rows={props.rows} columns={props.columns} pageSize= {props.pagesize} 
-                components={{Toolbar:CustomToolbar}}
-                onFilterModelChange={borrarFiltro}
-                onColumnHeaderClick={()=>console.log("click")}
-                filterModel={filtro || undefined}
-                />
-            </div>                
-        </>
-    )
+    const expandedRowRender = () => {
+        const columns: any[] = [
+            {dataIndex: 'ruc', title: 'Ruc', width: 160},
+            {dataIndex: 'empresa', title: 'Empresa', width: 170},      
+            {dataIndex: 'direccion', title: 'Direccion', width: 170},
+       
+          ];
+    
+        const rows = [
+        {
+        ruc: 1,
+        empresa: "Nathalie Andrea",
+        direccion: "Bohorquez Velez"       
+        },
+        {
+        ruc: 2,
+        empresa: "Nathalie Andrea",
+        direccion: "Bohorquez Velez"       
+        },
+        {
+        ruc: 3,
+        empresa: "Nathalie Andrea",
+        direccion: "Bohorquez Velez"       
+        }
+        ]
+        return <Table columns={columns} dataSource={rows} pagination={false} />;
+      };
+     
+      return (
+        <Table
+        className="components-table-demo-nested"
+        pagination  ={{pageSize : props.pagesize}}
+        style={{width:props.width}}
+        scroll ={{y:340}}
+        columns={props.columns}
+        expandable={{ expandedRowRender }}
+        dataSource={props.rows}
+        />
+      );
 
   
 }
