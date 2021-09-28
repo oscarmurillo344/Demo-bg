3bb407ecffbba0741e939fcee8e58f203efcc55a
Leonel Velez <leonel-14-velez@hotmail.com>
2021-09-28 16:43:19 -0500

    cambiando efecto de aparecer

--- src/componentes/menuBG/menuBG.tsx
+++ src/componentes/menuBG/menuBG.tsx
@@ -3,8 +3,8 @@ import './menuBG.css';
 import { bubble as MenuBurger } from 'react-burger-menu'
 import imagen from './../../jusLogo.png'
 import iconOption from "./../../iconLogOut.png"
-import { AiOutlineShop, AiOutlineSolution, AiOutlineUser, AiOutlineTeam } from "react-icons/ai";
-import { Dropdown, Menu } from 'antd';
+import { AiOutlineShop, AiOutlineSolution, AiOutlineUser, AiOutlineTeam, AiOutlineLogin, AiOutlineCaretDown } from "react-icons/ai";
+import { Button, Dropdown, Menu } from 'antd';
 import { AppstoreOutlined, DownOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
 import 'antd/dist/antd.css';
 import SubMenu from 'antd/lib/menu/SubMenu';
@@ -31,6 +31,11 @@ export default class MenuBG extends React.Component<MenuBGProps, MenuBGSatate>
     this.state ={moduloSeleccionado : ""}
  }
   
+  retornoBackground = (comparacion:string)=>
+  {
+    const color = comparacion.trim().toLowerCase() === this.state.moduloSeleccionado.trim().toLowerCase()? "#bc157c": "transparent" 
+    return color
+  }
   render()
   {
     return (<>
@@ -39,26 +44,47 @@ export default class MenuBG extends React.Component<MenuBGProps, MenuBGSatate>
 
             <img src= {imagen} width="40px" height="40px" ></img>
           </div>
-          
-
+          <div className='pantalla' >
+              
+              <p>Activos y Pasivos - Ahorros</p>
+          </div>
+          <div className="opciones" >
+          <Dropdown  placement="bottomRight" arrow overlay={
+              <Menu>
+              <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" href="#">
+                  Salir
+                </a>
+              </Menu.Item>
+              <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" href="#">
+                  Perfil
+                </a>
+              </Menu.Item>
+            </Menu>
+          }>
+                <div   className="flex boton" ><AiOutlineCaretDown></AiOutlineCaretDown></div>
+          </Dropdown>
+          </div>
         </nav>        
         <div className="container-menu ancho-manu flex colum" style={{alignItems:"center"}} >
             {
               this.props.items.map((recorre, index)=>{
-                return <a href="#" onClick={()=>this.setState({...this.state, moduloSeleccionado:recorre.nombre})}  className="flex container-item" style={{color:"white", fontSize:"30px"}} >
+                return <a href="#" onClick={()=>this.setState({...this.state, moduloSeleccionado:recorre.nombre})}  className="flex container-item" 
+                style={{color:"white", fontSize:"30px", backgroundColor: this.retornoBackground(recorre.nombre) }} >
                   {recorre.icon}
                 </a>
               })
             }
         </div>
-        <div className="container-menu-childrens" style={{display:this.state.moduloSeleccionado.trim() !== ""? "flex": "none"}} >
+        <div className="container-menu-childrens" style={{transform:this.state.moduloSeleccionado.trim() !== ""? "rotateY(0deg)": "rotateY(90deg)"}} >
             {
               this.props.items.filter(x=>x.nombre.trim().toLowerCase() === this.state.moduloSeleccionado.trim().toLowerCase()).map((recorre, index)=>{
                 {
                   return recorre.items.map((recorreChild:any, indexchild:any)=>{
                     return (
                       <> 
-                        <a href="#" className="container-menu-childrens-children" >
+                        <a href="#" className="container-menu-childrens-children"  >
                           
                           <div className="icon" >{recorreChild.icon}</div>
                           <div>{recorreChild.nombre}</div>
